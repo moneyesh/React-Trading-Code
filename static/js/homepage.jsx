ab3f72aa4e90352b967c9dfbca525188c7f6baf9
Iesha Cuadra <ieshacuadra@gmail.com>
2022-11-23 17:57:46 -0500

    Updated

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,59 @@
 'use strict';
 
+
+
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+
+{/* THIS FUNCTION is where we edit the Homepage component in 
+order to add some content to the homepage.html 
+
+Things to note:
+1.instead of putting return statement in a div(to make sure return returns only ONE thing, 
+  per convention, put i tin a react fragment tag so we dont have divs everywhere) https://reactjs.org/docs/fragments.html
+2. parenthesis are important!
+3. all tags must be closed...ex: input and image as the end include a "/" when using .jsx
+*/}
+
+
+
+
+  return (
+   
+    <React.Fragment>
+
+  {/* A welcome message to the user */}
+  <h3>welcome user!!!!</h3>
+
+  {/* A link to the /cards page */}
+  <a href="/cards">Click here to view trading cards!</a>
+  
+  {/* A nice, large image of Balloonicorn (the image is located at /static/img/balloonicorn.jpg) */}
+  <img src="/static/img/balloonicorn.jpg" />
+
+  </React.Fragment>
+
+  );
+
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
+
+
+
+
+
+
+
+//ReactDOM.render = WHAT TO PUT AND WHERE
+
+
+//we use ReactDOM.render to answer "WHAT to put?" ....to render the homepage component inside div#app....forward slash lets us know were calling the function
+// and the queryselector tells jsx "put it here"...its being referenced in homepage html via the div
+//2nd param: HTMLElement object. In this case, we retrieve the element with document.querySelector.
+
+
+// ReactDOM.render takes in two arguments:
+
+// The components to render
+// An HTML element — where you want to place this component
+
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -2,6 +2,80 @@
 
 const tradingCardData = [
   {
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
+  },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
+  },
+];
+
+
+//          1.Where is static/js/tradingCard.jsx getting pulled into the webpage?
+//--via script in line 6 of cards.html by way of js
+
+//          2.How does each trading card get mounted into the DOM?
+//--Iesha says -reactDOM.render..//ReactDOM.render = WHAT TO PUT AND WHERE
+
+//E says-trading card function
+
+//Each trading card gets mounted into the DOM in tradingCard.jsx via reactdom.render=the "WHAT"
+// For each card, an element is selected using document.querySelector=the "WHERE"
+// Then, an instance of the TradingCard component is created, providing 
+// different props for each trading card. The HTML for each trading card 
+// is provided by the render method on the TradingCard React component.
+
+//          3.Why is there only one TradingCard component definition, while there are three trading cards on the page itself?
+//iesha says-trading card is general 
+//e says-the one covers all three
+
+//          4.What are the props for each trading card? How are the props being rendered onto the page?
+//--name, skill and imgurl
+//--via componnent OR line 19 where it says const. 
+
+const tradingCardData = [
+  {
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg',
@@ -11,13 +85,23 @@ const tradingCardData = [
 function TradingCard(props) {
   return (
     <div className="card">
+      {/*  The HTML for each trading card 
+// is provided by the render method on the TradingCard React component. */}
+
+{/* We only have to define TradingCard once. It provides a blueprint of how 
+a trading card should look. This blueprint is then used to create a variety 
+of different trading cards.  */}
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
 
+
+// each ReactDOM are similar to instances
+//Then, an instance of the TradingCard component is created, providing 
+// different props for each trading card. 
 ReactDOM.render(
   <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
   document.querySelector('#balloonicorn'),
@@ -32,3 +116,20 @@ ReactDOM.render(
   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
   document.querySelector('#llambda'),
 );
+
+
+ReactDOM.render(
+  <TradingCard name="polymorphism" skill="juggling" imgUrl="/static/img/polymorphism.jpeg" />,
+  document.querySelector('#polymorphism'),
+);
+
+ReactDOM.render(
+<TradingCard name="seedpy" skill="gardening" imgUrl="/static/img/seedpy.jpeg" />, document.querySelector("#seedpy"),
+);
+
+
+//  think about how you would structure your trading card data in either Python or JavaScript.
+// via AJAX in some kind of tidy JSON data structure. 
+
+//DICTIONARYYY-iesha
+
